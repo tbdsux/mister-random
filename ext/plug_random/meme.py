b64e554b4ade5155e660576e68847f28207d2545
TheBoringDude <iamcoderx@gmail.com>
2020-09-10 22:47:42 +0800

    Some fixes

--- ext/plug_random/meme.py
+++ ext/plug_random/meme.py
@@ -3,31 +3,35 @@ import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = os.getenv("GOOGLE_CHROME_PATH")
-browser = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
-
 class Gagger:
     gag_website = "https://bit.ly/ShuffleNav"
 
     @staticmethod
     def get_gag():
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.binary_location = os.getenv("GOOGLE_CHROME_PATH")
+
+        browser = webdriver.Chrome(executable_path=os.getenv("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
         browser.get(Gagger.gag_website)
         source = browser.page_source
         browser.close()
+
         soup = BeautifulSoup(source, "html.parser")
-        container = soup.find("div", class_="post-container")
-
-        meme = ""
         try:
-            meme = container.find("source")["src"]
-        except Exception:
-            meme = container.find("source")["srcset"]
+            container = soup.find("div", class_="post-container")
 
-        return meme
+            meme = ""
+            try:
+                meme = container.find("source")["src"]
+            except Exception:
+                meme = container.find("source")["srcset"]
+
+            return meme
+        except Exception:
+            Gagger.get_gag()
 
 
 class Meme:
